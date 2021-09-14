d97a01d31f700272e87f811eb73f18a50bcd925d
Saroj Dahal <sarojdahal1213@gmail.com>
2021-09-14 18:13:36 +0545

    .

--- day3/modules/student.js
+++ day3/modules/student.js
@@ -1,17 +1,17 @@
-class Student {
+export default class Student {
   constructor(name, roll) {
     this.name = name;
     this.roll = roll;
   }
 }
 
-function getName(student) {
+export function getName(student) {
   console.log(`Student's Name is ${student.name}`);
 }
 
-function getRoll(student) {
+export function getRoll(student) {
   console.log(`Student's Roll is ${student.roll}`);
 }
 
-export default Student;
-export { getName, getRoll };
+// export default Student;
+// export { getName, getRoll };
--- day3/tomorrow.js
+++ day3/tomorrow.js
@@ -1,6 +1,6 @@
 //Tuesday
 //1. Callback Functions
+//5. Higher Order Functions
 //2. Classes & Inheritance.
 //3. Async / Await.
 //4. Promise
-//5. Higher Order Functions
